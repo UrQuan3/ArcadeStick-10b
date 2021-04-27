afe71f5f29b306633fce37a444dc9fb6e25e51ac
UrQuan3 <34602164+UrQuan3@users.noreply.github.com>
2021-04-26 20:46:36 -0500

    describe the pin mappings more clearly

--- code/ArcadeStick-10b/ArcadeStick-10b.ino
+++ code/ArcadeStick-10b/ArcadeStick-10b.ino
@@ -7,21 +7,21 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
+  pinMode(0, INPUT_PULLUP);   //up
+  pinMode(1, INPUT_PULLUP);   //right
+  pinMode(2, INPUT_PULLUP);   //down
+  pinMode(3, INPUT_PULLUP);   //left
+  pinMode(4, INPUT_PULLUP);   //button 1
+  pinMode(5, INPUT_PULLUP);   //button 2
+  pinMode(6, INPUT_PULLUP);   //button 3
+  pinMode(7, INPUT_PULLUP);   //button 4
+  pinMode(8, INPUT_PULLUP);   //button 5
+  pinMode(9, INPUT_PULLUP);   //button 6
+  pinMode(10, INPUT_PULLUP);  //button 7
+  pinMode(11, INPUT_PULLUP);  //button 8
   pinMode(13, OUTPUT);
-  pinMode(14, INPUT_PULLUP);
-  pinMode(15, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);  //button 9  (start)
+  pinMode(15, INPUT_PULLUP);  //button 10 (select/insert coin)
   digitalWrite(13, HIGH);
 }
 
